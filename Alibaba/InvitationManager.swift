6f012b12b14719b4e841dc7e63b41dee79248b94
s.murakami <smurakami.84@gmail.com>
2016-07-02 19:55:28 +0900

    Get JSON

--- Alibaba/InvitationManager.swift
+++ Alibaba/InvitationManager.swift
@@ -11,26 +11,31 @@ import UIKit
 class InvitationManager: NSObject {
     
     
-    private var json:NSDictionary!
+    private var json = NSArray()
     
     func getJson() {
         
-        let URL: NSURL = NSURL(string: "http://express.heartrails.com/api/json?method=getStations&name=%E6%96%B0%E5%AE%BF")!
-        let jsonData :NSData = NSData(contentsOfURL: URL)!
+        let URL: NSURL = NSURL(string: "http://10.201.120.98:3000/invites")!
+        guard let jsonData :NSData = NSData(contentsOfURL: URL) else {
+            
+            return
+        }
         
         do {
-            json = try NSJSONSerialization.JSONObjectWithData(jsonData, options: .MutableContainers) as! NSDictionary
+            json = try NSJSONSerialization.JSONObjectWithData(jsonData, options: .MutableContainers) as! NSArray
         } catch  {
             // エラー処理
         }
         
-        let response:NSDictionary = json.objectForKey("response") as! NSDictionary
-        let station:NSArray = response.objectForKey("station") as! NSArray
+        print(json)
         
-        
-        for i in 0 ..< station.count {
-            print(station[i].objectForKey("prefecture") as! NSString)
-        }
+//        let response:NSDictionary = json.objectForKey("response") as! NSDictionary
+//        let station:NSArray = response.objectForKey("station") as! NSArray
+//        
+//        
+//        for i in 0 ..< station.count {
+//            print(station[i].objectForKey("prefecture") as! NSString)
+//        }
     }
 
 
