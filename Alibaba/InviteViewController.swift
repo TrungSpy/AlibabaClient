b16fb0dca61248e5e07b2f7e019673824ca86288
s.murakami <smurakami.84@gmail.com>
2016-07-02 22:11:50 +0900

    Added view controller classes

---
+++ Alibaba/InviteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InviteViewController.swift
+//  Alibaba
+//
+//  Created by 村上晋太郎 on 2016/07/02.
+//  Copyright © 2016年 S. Murakami. All rights reserved.
+//
+
+import UIKit
+
+class InviteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
