ce43b8b7deef8732495052e00cce49ffffd223fe
s.murakami <smurakami.84@gmail.com>
2016-07-03 13:14:31 +0900

    button

--- Alibaba/MessageView.swift
+++ Alibaba/MessageView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MessageView: UIView {
     
+    var direction: Direction = .Up
+    
     enum Direction {
         case Up, Down
     }
@@ -40,6 +42,11 @@ class MessageView: UIView {
     class func instance(iconType: Int, pos: CGPoint, direction: Direction) -> MessageView {
         let view = MessageView(frame: CGRectMake(0, 0, 66, 66))
         view.center = pos
+        
+        let imageView = UIImageView(image: UIImage(named: Message.imageNames[iconType]) ?? UIImage())
+        view.addSubview(imageView)
+        view.direction = direction
+        
         return view
     }
 
